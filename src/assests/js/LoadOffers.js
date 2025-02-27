a752f6e5f9c7d7ff4570cad87573cca90e6ef073
Eduardo <edaniel2004gainza@gmail.com>
2025-02-27 23:15:20 +0000

    Footer section and new images added

--- src/assests/js/LoadOffers.js
+++ src/assests/js/LoadOffers.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const offersList = document.getElementById("OffersList");
-    const numOfertas = 9; // Número de ofertas a generar
+    const numOfertas = 6;
 
     for (let i = 0; i < numOfertas; i++) {
         let li = document.createElement("li");
